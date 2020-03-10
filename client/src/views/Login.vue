83bec22a6d878bd27501a464e6508939f9ead1cc
fr.Innocent <brother.symeon@gmail.com>
2020-03-10 09:31:18 -0400

    login to server works

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -10,17 +10,17 @@
       <v-form
     ref="form"
     v-model="valid"
-    @submit.prevent="login"
+    @submit.prevent="login(valid, user)"
   >
     <v-text-field
-      v-model="email"
+      v-model="user.email"
       :counter="10"
       :rules="emailRules"
       label="Email"
     ></v-text-field>
 
     <v-text-field
-      v-model="password"
+      v-model="user.password"
       :rules="passwordRules"
       label="Password"
     ></v-text-field>
@@ -30,6 +30,13 @@
 
 
   </v-form>
+  <v-progress-circular
+          v-if="loading"
+          :size="70"
+          :width="7"
+          indeterminate
+          color="primary">
+        </v-progress-circular>
     </v-layout>
     </v-slide-y-transition>
   </v-container>
@@ -37,24 +44,48 @@
 
 <script>
 /* eslint-disable */
+import { mapState, mapActions } from 'vuex';
 export default {
 name: 'login',
   data: () => ({
-    valid: true,
-    email:'',
+    user: {
+      email: '',
+      password: ''
+    },
+    valid: false,
     emailRules: [
       value => !!value || 'Email is required',
       value => /.+@.+\..+/.test(value) || 'Email must be valid'
     ],
-    password: '',
     passwordRules: [
-      value => (value && value.length < 20 && value.length > 12) || 'Password must have a length of greater than 12 and less than 20'
+      value => (value && value.length < 20 && value.length > 9) || 'Password must have a length of greater than 12 and less than 20'
     ],
   }),
+  computed: {
+    ...mapState('auth', { loading: 'isAuthenticatePending' } ),
+  },
   methods: {
-   login(){
-    
-   }
+    ...mapActions('auth', ['authenticate']),
+    login(valid, user){
+      const { email, password } = user
+      this.authenticate({ strategy: 'local', email, password })
+        .then(user => {
+          console.log('authenticated user=', user)
+          this.$router.push('/')
+        })
+        .catch(err => {
+          // Convert the error to a plain object and add a message.
+          let type = err.className
+          err = Object.assign({}, err)
+          err.message =
+            type === 'not-authenticated'
+              ? 'Incorrect email or password.'
+              : 'An error prevented login.'
+          this.error = err
+          console.log(this.error)
+        })
+    }
   }
+  
 }
 </script>
